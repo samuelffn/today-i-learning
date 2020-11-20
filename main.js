b9752075090434b00f2275abf3c59df845506a04
samuelffn <samuelffn@gmail.com>
2020-11-19 21:48:39 -0300

    Utilizando classes e herança

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-alert('Teste de babel');
+class List {
+    constructor() {
+        this.data = [];
+    }
+
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
+    }
+}
+class ItensList extends List {
+    /* constructor() {
+        this.itens = [];
+    }
+
+    addItens() {
+        this.itens.push("Novo item");
+        console.log(this.itens);
+    } */
+
+    constructor() {
+        super();
+        this.usuario = 'Samuel';
+    }
+
+    exibeUsuario() {
+        console.log(this.usuario);
+    }
+}
+
+const meusItens = new ItensList();
+
+document.getElementById('novoitem').onclick = function() {
+    //meusItens.addItens();
+    meusItens.add("Novo item");
+}
+
+meusItens.exibeUsuario();
