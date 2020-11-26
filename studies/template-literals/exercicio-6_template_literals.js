e2d751414131a04be07ad840fbddaee961effdb2
Samuel França <samuelffn@gmail.com>
2020-11-26 16:58:05 -0300

    Add files via upload

---
+++ studies/template-literals/exercicio-6_template_literals.js
@@ -0,0 +1,11 @@
+// Ecxercício 6
+/**
+    Converta o seguinte trecho de código utilizando Template Literals:
+    const usuario = 'Diego';
+    const idade = 23;
+    console.log('O usuário ' + usuario + ' possui ' + idade + ' anos');
+*/
+
+const usuario = 'Diego';
+const idade = 23;
+console.log(`O usuário ${usuario} possui ${idade} anos`);
