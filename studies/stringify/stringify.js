ef5bab019b50809af0860b5e0d7f557b97604f3d
Samuel França <samuelffn@gmail.com>
2020-12-08 01:10:25 -0300

    Update stringify.js

--- studies/stringify/stringify.js
+++ studies/stringify/stringify.js
@@ -23,6 +23,18 @@ JSON.stringify({ [Symbol.for('foo')]: 'foo' }, function(k, v) {
 });
 // '{}'
 
+// Exemplo de uma função
+function replacer(key, value) {
+  if (typeof value === "string") {
+    return undefined;
+  }
+  return value;
+}
+
+var foo = {fundação: "Mozilla", modelo: "caixa", semana: 45, transporte: "carro", mês: 7};
+var jsonString = JSON.stringify(foo, replacer);
+//O resultado do JSON é:  {"semana":45,"mês":7}.
+
 // Exemplo com Array
 JSON.stringify(foo, ['week', 'month']);  
 // '{"week":45,"month":7}', only keep "week" and "month" properties
@@ -49,3 +61,31 @@ var obj = {
 };
 JSON.stringify(obj);        // '"bar"'
 JSON.stringify({ x: obj }); // '{"x":"bar"}'
+
+// Examplo de uso de JSON.stringify() com localStorage
+// Criando um exemplo em JSON
+var seção = {
+  'telas': [],
+  'estado': true
+};
+session.screens.push({ 'nome': 'telaA', 'largura': 450, 'altura': 250 });
+session.screens.push({ 'nome': 'telaB', 'largura': 650, 'altura': 350 });
+session.screens.push({ 'nome': 'telaC', 'largura': 750, 'altura': 120 });
+session.screens.push({ 'nome': 'telaD', 'largura': 250, 'altura': 60 });
+session.screens.push({ 'nome': 'telaE', 'largura': 390, 'altura': 120 });
+session.screens.push({ 'nome': 'telaF', 'largura': 1240, 'altura': 650 });
+
+// Convertendo a string JSON em JSON.stringify()
+// Salvando com localStorage no nome da sessão
+localStorage.setItem('seção', JSON.stringify(seção));
+
+// Exemplo de como transformar a String gerada por meio de: 
+// JSON.stringify() e salva em localStorage no objeto JSON novamente
+var seçãoRestaurada = JSON.parse(localStorage.getItem('seção'));
+
+// Agora, a variável seçãoRestaurada contém o objeto que foi salvo
+// no localStorage
+console.log(seçãoRestaurada);
+
+
+
