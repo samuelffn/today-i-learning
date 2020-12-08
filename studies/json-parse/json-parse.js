5991ad02d8280124027ddf27b14e5a3cc1d7a448
Samuel França <samuelffn@gmail.com>
2020-12-08 01:13:13 -0300

    Update json-parse.js

--- studies/json-parse/json-parse.js
+++ studies/json-parse/json-parse.js
@@ -6,3 +6,42 @@ console.log(obj.count);
 
 console.log(obj.result);
 // expected output: true
+
+// Usando JSON.parse()
+JSON.parse('{}');              // {}
+JSON.parse('true');            // true
+JSON.parse('"foo"');           // "foo"
+JSON.parse('[1, 5, "false"]'); // [1, 5, "false"]
+JSON.parse('null');            // null
+
+
+// Usando o parâmetro reviver
+JSON.parse('{"p": 5}', (key, value) =>
+  typeof value === 'number'
+    ? value * 2 // retorna o valor * 2 para números
+    : value     // retorna tudo sem alteração
+);
+
+// { p: 10 }
+
+JSON.parse('{"1": 1, "2": 2, "3": {"4": 4, "5": {"6": 6}}}', (key, value) => {
+  console.log(key); // mostra o nome da propriedade atual, o último é "".
+  return value;     // retorna o valor da propriedade inalterada.
+});
+
+// 1
+// 2
+// 4
+// 6
+// 5
+// 3 
+// ""
+
+
+// JSON.parse() não é permitido vírgulas à direta
+// ambos retornarão um SyntaxError
+JSON.parse('[1, 2, 3, 4, ]');
+JSON.parse('{"foo" : 1, }');
+
+
+
