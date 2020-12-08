2a9fae2aa0c89cf2477c30006ac8d3288f40e0de
Samuel França <samuelffn@gmail.com>
2020-12-08 01:08:18 -0300

    Update stringify.js

--- studies/stringify/stringify.js
+++ studies/stringify/stringify.js
@@ -22,3 +22,30 @@ JSON.stringify({ [Symbol.for('foo')]: 'foo' }, function(k, v) {
   }
 });
 // '{}'
+
+// Exemplo com Array
+JSON.stringify(foo, ['week', 'month']);  
+// '{"week":45,"month":7}', only keep "week" and "month" properties
+
+// Argumento Space
+JSON.stringify({ a: 2 }, null, ' ');
+// '{
+//  "a": 2
+// }'
+
+JSON.stringify({ uno: 1, dos: 2 }, null, '\t');
+// r:
+// '{
+//     "uno": 1,
+//     "dos": 2
+// }'
+
+// COmportamento do toJson
+var obj = {
+  foo: 'foo',
+  toJSON: function() {
+    return 'bar';
+  }
+};
+JSON.stringify(obj);        // '"bar"'
+JSON.stringify({ x: obj }); // '{"x":"bar"}'
