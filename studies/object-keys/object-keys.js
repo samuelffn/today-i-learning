dcf0ba2169ffc3335ad7bf94152aa3aae8b0b81e
Samuel França <samuelffn@gmail.com>
2021-02-10 15:36:13 -0300

    Update object-keys.js

--- studies/object-keys/object-keys.js
+++ studies/object-keys/object-keys.js
@@ -4,7 +4,7 @@ console.log(Object.keys(arr)); // console: ['0', '1', '2']
 
 // Array com objeto
 const obj = { 0: 'a', 1: 'b', 2: 'c', 3: { d: '4', e: '5' } };
-console.log(Object.keys(obj)); // console: ['0', '1', '2']
+console.log(Object.keys(obj)); // console: ['0', '1', '2', '3']
 
 // Array como objeto com ordenação aleatória por chave
 const an_obj = { 100: 'a', 2: 'b', 7: 'c' };
