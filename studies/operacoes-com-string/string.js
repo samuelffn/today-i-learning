67f357b5d959304223d129d50720f2038ca7f60d
Samuel França <samuelffn@gmail.com>
2022-02-10 02:24:12 -0300

    Update string.js

--- studies/operacoes-com-string/string.js
+++ studies/operacoes-com-string/string.js
@@ -6,7 +6,9 @@ const cpfLimpo = cpf.replace(/\D/g, '');
 console.log('cpf: ', cpf); //123.456.789-00
 console.log('cpfLimpo: ', cpfLimpo); //12345678900
 
+
 /* Setando uma máscara numa variável */
+
 const cpf = '123.456.789-00';
 const addMaskCpf = cpfLimpo.replace(
   /(\d{3})(\d{3})(\d{3})(\d{2})/,
