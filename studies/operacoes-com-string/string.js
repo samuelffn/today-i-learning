5f727720c5e2fe88fd85029b0eefc929ad8a5861
Samuel França <samuelffn@gmail.com>
2022-02-10 02:28:25 -0300

    Update string.js

--- studies/operacoes-com-string/string.js
+++ studies/operacoes-com-string/string.js
@@ -1,5 +1,4 @@
 /* Fazendo replace nas strings */
-
 const cpf = '123.456.789-00';
 const cpfLimpo = cpf.replace(/\D/g, '');
 
@@ -8,10 +7,8 @@ console.log('cpfLimpo: ', cpfLimpo); //12345678900
 
 
 /* Setando uma máscara numa variável */
+const cpf2 = '12345678901';
+const addMaskCpf = cpf2.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
 
-const cpf2 = '123.456.789-00';
-const addMaskCpf = cpf2.replace(
-  /(\d{3})(\d{3})(\d{3})(\d{2})/,
-  '$1.$2.$3-$4'
-);
-console.log('cpfWithMask: ', addMaskCpf); //123.456.789-00
+console.log('cpf2: ', cpf2); //12345678901
+console.log('cpfWithMask: ', addMaskCpf); //123.456.789-01
